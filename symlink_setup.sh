b11afb633bab3c75e52e52d06608a94fa8a94ee4
Noah Wahl <noah.ares@yahoo.de>
2020-12-15 14:46:07 +0100

    Added xdg dirs and xinitrc

--- symlink_setup.sh
+++ symlink_setup.sh
@@ -8,8 +8,10 @@ rm -r ~/.config/sxhkd
 rm -r ~/.config/polybar
 rm -r ~/.config/nvim
 rm -r ~/.config/bspwm
-rm -r ~/.config/vifm
 rm -r ~/.config/zsh
+[ ! -d ~/.config/qutebrower ] && mkdir ~/.config/qutebrowser
+[ ! -d ~/.config/vifm ] && mkdir ~/.config/vifm
+[ ! -d ~/.config/X11 ] && mkdir ~/.config/X11
 
 ln -s ~/dotfiles-base/zathura ~/.config/zathura
 ln -s ~/dotfiles-base/dunst ~/.config/dunst
@@ -17,7 +19,11 @@ ln -s ~/dotfiles-base/sxhkd ~/.config/sxhkd
 ln -s ~/dotfiles-base/polybar ~/.config/polybar
 ln -s ~/dotfiles-base/nvim ~/.config/nvim
 ln -s ~/dotfiles-base/bspwm ~/.config/bspwm
-ln -s ~/dotfiles-base/vifm ~/.config/vifm
+ln -s ~/dotfiles-base/vifm/colors ~/.config/vifm/colors
+ln -s ~/dotfiles-base/vifm/scripts ~/.config/vifm/scripts
+ln -s ~/dotfiles-base/vifm/vifmrc ~/.config/vifm/vifmrc
 ln -s ~/dotfiles-base/qutebrowser/config.py ~/.config/qutebrowser/config.py
 ln -s ~/dotfiles-base/zsh/.zshenv ~/.zshenv
 ln -s ~/dotfiles-base/zsh ~/.config/zsh
+ln -s ~/dotfiles-base/user-dirs.dirs ~/.config/user-dirs.dirs
+ln -s ~/dotfiles-base/xinitrc ~/.config/X11/xinitrc
