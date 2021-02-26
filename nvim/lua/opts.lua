e7889d33e4ff5c1aa2c84231f4480461f9bace7c
Noah Wahl <noah.ares@yahoo.de>
2021-02-26 13:02:08 +0100

    Set options correctly

--- nvim/lua/opts.lua
+++ nvim/lua/opts.lua
@@ -1,40 +1,40 @@
 local g = vim.g
 local o = vim.o
-local b = vim.bo
-local w = vim.wo
+local utils = require('utils')
 
 g.mapleader = ','
+g.tex_flavor = "latex"
 
-w.number = true
-w.relativenumber = true
+utils.opt('w', 'number', true)
+utils.opt('w', 'relativenumber', true)
 -- faster macros
-o.lazyredraw = true
+utils.opt('o', 'lazyredraw', true)
 -- matching parenthesis
-o.showmatch = true
+utils.opt('o', 'showmatch', true)
 -- switch buffer without saving them
-o.hidden = true
+utils.opt('o', 'hidden', true)
 -- better searching
-o.ignorecase = true
-o.smartcase = true
-o.hlsearch = false
+utils.opt('o', 'ignorecase', true)
+utils.opt('o', 'smartcase', true)
+utils.opt('o', 'hlsearch', false)
 
 -- show lines bellow cursor
-o.scrolloff = 5
-o.sidescrolloff = 5
+utils.opt('o', 'scrolloff', 5)
+utils.opt('o', 'sidescrolloff', 5)
 
 -- tab config
-o.expandtab = true
-o.shiftwidth = 2
-o.tabstop = 2
-o.softtabstop = 2
+utils.opt('b', 'expandtab', true)
+utils.opt('b', 'shiftwidth', 2)
+utils.opt('b', 'tabstop', 2)
+utils.opt('b', 'softtabstop', 2)
 
 -- split in reasonable positions
-o.splitright = true
-o.splitbelow = true
+utils.opt('o', 'splitright', true)
+utils.opt('o', 'splitbelow', true)
 
 --folds
-w.foldmethod = 'expr'
-w.foldexpr = 'nvim_treesitter#foldexpr()'
-o.foldlevelstart = 99
+utils.opt('w', 'foldmethod', 'expr')
+utils.opt('w', 'foldexpr', 'nvim_treesitter#foldexpr()')
+utils.opt('o', 'foldlevelstart', 99)
 
 o.formatoptions = o.formatoptions:gsub("r", ""):gsub("o", "")
