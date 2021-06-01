baaae20a8966eaca71eb88f5065e71d973bba30b
Noah Wahl <noah.ares@yahoo.de>
2021-06-01 15:06:18 +0200

    vimwiki config

---
+++ nvim/lua/vimwiki.lua
@@ -0,0 +1,11 @@
+local g = vim.g
+
+g.vimwiki_list = {{
+  path = '~/Nextcloud/vimwiki/',
+  syntax = 'markdown',
+  ext = '.md',
+  links_space_char = '_'
+}}
+
+g.vimwiki_ext2syntax = {md = 'markdown'}
+g.vimwiki_auto_header = 1
