940d32ebe2a67bd4d66689f545799819f41d14a6
Noah Wahl <noah.ares@yahoo.de>
2021-12-11 12:46:55 +0100

    Fix cd to root

--- nvim/lua/project_root.lua
+++ nvim/lua/project_root.lua
@@ -4,10 +4,12 @@ function find_project_root()
   local id = [[.git]]
   local file = api.nvim_buf_get_name(0)
   local root = vim.fn.finddir(id, file .. ';')
+  root = root:gsub(id, '')
   if root ~= "" then
-    root = root:gsub(id, '')
     print(root)
     vim.api.nvim_set_current_dir(root)
+  elseif root ~= vim.fn.getcwd() then
+    print("Already at root of project.")
   else
     print("No repo found.")
   end
